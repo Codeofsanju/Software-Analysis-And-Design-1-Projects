c3fe8b6fcae258b83896eacf12ad21681b1ef06e
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2017-05-26 23:25:20 -0400

    Add files via upload

---
+++ Lab2 - Simple file processing using redirection/largestInt.cpp
@@ -0,0 +1,9 @@
+//Author: Sanjeev Sharma
+//Task: This program reads the file redirected to standard input. The file contains positive integers. After reading the file, the program prints out the largest integer found in the file. 
+//Purpose: Practice taking input from file using file redirection
+#include <iostream>
+using namespace std;
+
+int main (){
+
+}
